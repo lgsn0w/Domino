e6b2788e933da810bc97cec7c9acf5658ef24a1e
Lucas <112468011+lgsn0w@users.noreply.github.com>
2023-08-25 03:22:55 -0300

    Update board.py
    TODOs adicionados

--- board.py
+++ board.py
@@ -3,6 +3,19 @@ class Tabuleiro:
         self.pecas_esquerda = []
         self.pecas_direita = []
 
+
+     #TODO LISTA:
+     #1 - ARRANJAR ALGUM JEITO DE IMPRIMIR O TABULEIRO DE UMA FORMA MAIS BONITA OU CENTRALIZADA
+     #2 - ADICIONAR CHECAGEM NOS METODOS DE ADICIONAR PEÇA PARA VER SE A PEÇA É VALIDA
+     #3 - DESCOBRIR COMO INICIALIZAR COM 6,6 A PARTIR DA PILHA DE PEÇAS (SEM DAR ERRO)
+     #4 - ACERTAR A DIREÇÃO DO FLUXO DE JOGO (<- ESQUERDA DIREITA -> |||||  AO INVES DE <- <-)
+     #5 - ME MATAR (IMPORTANTE!!!!)
+    
+
+    def adicionarPecaInicial(self, peca):
+        self.pecas_esquerda.append(peca)
+        self.pecas_direita.append(peca)
+
     def adicionarPecaEsquerda(self, peca):
         self.pecas_esquerda.insert(0, peca)
 
@@ -11,8 +24,13 @@ class Tabuleiro:
 
     def printTabuleiro(self):
         print("Tabuleiro:")
-        self._printBranch(self.pecas_esquerda[::-1])
-        print(f" {self.pecas_esquerda[-1][0]}-{self.pecas_esquerda[-1][1]} ", end="->")
+
+        if self.pecas_esquerda:
+            self._printBranch(self.pecas_esquerda[::-1])  # Print the left branch in reverse order
+            print(f" {self.pecas_esquerda[-1][0]}-{self.pecas_esquerda[-1][1]}", end=" ")
+
+
+
         self._printBranch(self.pecas_direita)
         print("\n")
 
